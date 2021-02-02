02f2e853b20671d1d29fb4055e604b06c042a3de
ane <anebzt@protonmail.com>
2021-02-02 20:41:01 +0100

    minimal streamlit app for plot of average data

--- average_data.py
+++ average_data.py
@@ -1,27 +1,20 @@
 import pandas as pd
 from datetime import datetime
+import matplotlib.pyplot as plt
 
-boulder_data = pd.read_csv("~/Desktop/coding/boulder_data/boulderdata.csv")
-
-def avg_data_day(chosen_day, chosen_gym):
+def avg_data_day(boulderdf, day, gym):
     """averages all of the data for a specific gym, day, and time of the week and returns a new df"""
-
-    #returns a number for the chosen day
-    chosen_day = chosen_day.lower()
-    day_dict = {'monday': 0, 'tuesday': 1, 'wednesday': 2, 'thursday': 3, 'friday': 4, 'saturday': 5,'sunday': 6}
-    day_num = day_dict[chosen_day]
     
     # returns all data for a specific day
     list_of_data = []
-    for x in boulder_data.current_time.unique():
-        date = x
-        day = datetime.strptime(date, '%Y/%m/%d %H:%M').weekday()
-        if day == day_num:
-            list_of_data.append(x)
-    ave_day_data = boulder_data[boulder_data['current_time'].isin(list_of_data)]
+    for date in boulderdf.current_time.unique():
+        local_day = datetime.strptime(date, '%Y/%m/%d %H:%M').weekday()
+        if local_day == day:
+            list_of_data.append(date)
+    ave_day_data = boulderdf[boulderdf['current_time'].isin(list_of_data)]
 
     # filters df for specific gym
-    ave_data = ave_day_data[ave_day_data['gym_name'] == chosen_gym]
+    ave_data = ave_day_data[ave_day_data['gym_name'] == gym]
 
     # clean df to have time in a separate column, prep for next step
     ave_data[['date', 'time']] = ave_data['current_time'].str.split(' ', 1, expand=True)
@@ -51,10 +44,10 @@ def avg_data_day(chosen_day, chosen_gym):
     return final_df
 
 
-if __name__ == "__main__":
-
-    chosen_day = input("Please select a day of the week: ")
-    print()
-    print(boulder_data.gym_name.unique())
-    chosen_gym = input("Please select a gym: ")
-    ave_data = avg_data_day(chosen_day, chosen_gym)
+def plot_ave_data(df):
+    fig = plt.figure()
+    plt.plot('time', 'occupancy', data=df, marker='o', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=4)
+    plt.plot('time', 'waiting', data=df, marker='', color='olive', linewidth=2)
+    plt.plot('time', 'weather_temp', data=df, marker='', color='olive', linewidth=2, linestyle='dashed', label="temp")
+    plt.legend()
+    return fig
---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import streamlit as st
+from average_data import avg_data_day, plot_ave_data
+
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+gyms = ['Munich East', 'Munich West', 'Regensburg', 'Dortmund', 'Frankfurt']
+gyms_dict = {'Munich East': 'muenchen-ost', 'Munich West': 'muenchen-west', 'Regensburg': 'regensburg', 'Dortmund': 'dortmund', 'Frankfurt': 'frankfurt'}
+
+if __name__ == "__main__":
+
+    st.title('Boulder gym tracker')
+    boulderdf = pd.read_csv("boulderdata.csv")
+
+    gym = st.selectbox('Select gym', gyms)
+    day = st.selectbox('Select day of the week', weekdays)
+
+    ave_data = avg_data_day(boulderdf, weekdays.index(day), gyms_dict[gym])
+
+    st.write("Plotting average occupancy, waiting people and weather")
+    st.pyplot(plot_ave_data(ave_data))
