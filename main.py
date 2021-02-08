5d70d1b8b001a865a552eff2b43558761bc81f19
ane <anebzt@protonmail.com>
2021-02-08 20:09:48 +0100

    show current day as default

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import datetime
 import pandas as pd
 import streamlit as st
 from src.average_data import avg_data_day, plot_ave_data
 
 weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-gyms = ['Munich East', 'Munich West', 'Regensburg', 'Dortmund', 'Frankfurt']
-gyms_dict = {'Munich East': 'muenchen-ost', 'Munich West': 'muenchen-west', 'Regensburg': 'regensburg', 'Dortmund': 'dortmund', 'Frankfurt': 'frankfurt'}
+gyms = ['Munich East', 'Munich West', 'Dortmund', 'Frankfurt', 'Regensburg']
+gyms_dict = {'Munich East': 'muenchen-ost', 'Munich West': 'muenchen-west', 'Dortmund': 'dortmund', 'Frankfurt': 'frankfurt', 'Regensburg': 'regensburg'}
 
 if __name__ == "__main__":
 
@@ -12,7 +13,7 @@ if __name__ == "__main__":
     boulderdf = pd.read_csv("boulderdata.csv")
 
     gym = st.selectbox('Select gym', gyms)
-    day = st.selectbox('Select day of the week', weekdays)
+    day = st.selectbox('Select day of the week', weekdays, index=datetime.datetime.today().weekday())
 
     avgdf = avg_data_day(boulderdf, weekdays.index(day), gyms_dict[gym])
 
--- src/average_data.py
+++ src/average_data.py
@@ -36,19 +36,11 @@ def avg_data_day(boulderdf: pd.DataFrame, day: int, gym: str) -> pd.DataFrame:
 
 def plot_ave_data(df: pd.DataFrame) -> go.Figure:
     fig = go.Figure()
-
-    fig.add_trace(go.Scatter(x=df.time, y=df.occupancy, name='Occupancy',
-                         line=dict(color='firebrick', width=4)))
-    fig.add_trace(go.Scatter(x=df.time, y=df.waiting, name='Waiting',
-                         line=dict(color='royalblue', width=4,
-                              dash='dash'))) # dash options include 'dash', 'dot', and 'dashdot
-    fig.add_trace(go.Scatter(x=df.time, y=df.weather_temp, name='Weather Temp',
-                         line = dict(color='green', width=4, dash='dot')))
-
-    fig.update_layout(title='Plotting average occupancy, waiting people and weather',
-                   xaxis_title='Time')
+    # dash options include 'dash', 'dot', and 'dashdot
+    fig.add_trace(go.Scatter(x=df.time, y=df.occupancy, name='Occupancy', line=dict(color='firebrick', width=4)))
+    fig.add_trace(go.Scatter(x=df.time, y=df.waiting, name='Waiting', line=dict(color='royalblue', width=4, dash='dash')))
+    fig.add_trace(go.Scatter(x=df.time, y=df.weather_temp, name='Weather Temp', line = dict(color='green', width=4, dash='dot')))
+    fig.update_layout(title='Plotting average occupancy, waiting people and weather', xaxis_title='Time')
 
     fig['layout']['yaxis'].update(title='', range=[-5, 105], autorange=False)
-
-
     return fig
