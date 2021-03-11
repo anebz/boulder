bfdb355d1cb7ecacf7364bebafc5b30588ff2675
ane <anebzt@protonmail.com>
2021-03-11 17:57:52 +0100

    discarding cron idea

--- scrape.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-scrapy crawl boulder -o output.csv
-python src/merge_df.py
-aws s3 cp boulderdata.csv s3://bboulderdataset/boulderdata.csv
--- src/clock.py
+++
@@ -1,9 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-sched = BlockingScheduler()
-
-@sched.scheduled_job('cron', hour='7-23', minute='*/15')
-def scheduled_job():
-    print('This job is run every 15mins between 7:00 and 23:00.')
-
-sched.start()
