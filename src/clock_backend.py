ecf4be6e05216a9c10a34b916e03c34cfcc4db3d
ane <anebzt@protonmail.com>
2021-03-30 10:38:59 +0200

    trying to see logs in heroku

--- src/clock_backend.py
+++ src/clock_backend.py
@@ -1,6 +1,5 @@
-import os
+import sys
 import boto3
-import subprocess
 import pandas as pd
 from apscheduler.schedulers.blocking import BlockingScheduler
 from web_scrape import scrape_websites
@@ -14,11 +13,13 @@ dfname = 'boulderdata.csv'
 @sched.scheduled_job('cron', hour='7-23', minute='*/15')
 def run_backend():
     print("Scraping websites")
+    sys.stdout.flush()
     webdf = scrape_websites()
 
     # only update if occupancy in gyms is > 0
     if (webdf['occupancy'] == 0).all():
         print("Nothing was scraped, S3 is not updated")
+        sys.stdout.flush()
         return
 
     # download dataset from S3
@@ -27,9 +28,11 @@ def run_backend():
     webdf.append(pd.read_csv(dfname)).to_csv(dfname, index=False)
     s3.upload_file(dfname, bucketname, dfname)
     print("Scraping done and data updated to S3")
+    sys.stdout.flush()
     return
 
 
 if __name__ == "__main__":
     print("Backend module starting")
+    sys.stdout.flush()
     sched.start()
