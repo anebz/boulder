a230e59f37e1a2e916342a3dffe464a8103473b9
Ane <anebzt@protonmail.com>
2021-09-06 20:05:46 +0200

    fix formatting

--- fix_missing_values.py
+++ fix_missing_values.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Aug 21 19:05:52 2021
-
 @author: Anja
 """
 
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.interpolate import interp1d
 import numpy as np
+import pandas as pd
+from scipy.interpolate import interp1d
 
 
-#There are two possible cases: 
-#    a) the value was not recorded at the given time
-#    b) the value was recorded too late
-
-def get_missing_aquisition_timestamps(df_gym, interval = '20min', sample_start = '07:20', sample_end = '23:40'):
+# 
+def get_missing_aquisition_timestamps(df_gym, interval='20min', sample_start='07:20', sample_end='23:40'):
     '''
-    Get which timestamps are missing for each day
+    Get which timestamps are missing for each day. There are two possible cases:
+    a) the value was not recorded at the given time
+    b) the value was recorded too late
 
     Parameters
     ----------
@@ -36,26 +33,16 @@ def get_missing_aquisition_timestamps(df_gym, interval = '20min', sample_start =
         DESCRIPTION.
     values_recorded_too_late : TYPE
         DESCRIPTION.
-
     '''
-    series = pd.Series(data = np.array(df_gym.occupancy),
-                       index = df_gym.current_time)
-    series_o = pd.Series(data = np.array(df_gym.occupancy),
-                         index = df_gym.current_time)
+    series = pd.Series(data=np.array(df_gym.occupancy), index=df_gym.current_time)
+    series_o = pd.Series(data=np.array(df_gym.occupancy), index=df_gym.current_time)
     #add a point at the start and end time if it is not starting at the start and end time of the day!
-    series_startpoint = pd.Series(data = [None],
-                                  index = [df_gym.current_time.min().replace(hour = int(sample_start[:2]),
-                                                                                  minute = int(sample_start[3:]))
-                                                ])
-    series_endpoint = pd.Series(data = [None], 
-                                index = [df_gym.current_time.max().replace(hour = int(sample_end[:2]),
-                                                                                  minute = int(sample_end[3:]))
-                                                ])
+    series_startpoint = pd.Series(data=[None], index=[df_gym.current_time.min().replace(hour=int(sample_start[:2]),
+                                                                                        minute=int(sample_start[3:]))])
+    series_endpoint = pd.Series(data=[None], index=[df_gym.current_time.max().replace(hour=int(sample_end[:2]),
+                                                                                      minute=int(sample_end[3:]))])
     try:
         series = series.append(series_endpoint, verify_integrity=True)
-    except ValueError:
-        pass
-    try:
         series = series.append(series_startpoint, verify_integrity=True)
     except ValueError:
         pass
@@ -64,13 +51,11 @@ def get_missing_aquisition_timestamps(df_gym, interval = '20min', sample_start =
     #upsample it
     series_upsampled = series.resample(interval).asfreq()
     #there are times when there should be no sampling.
-    series_upsampled = series_upsampled.between_time(sample_start,
-                                                     sample_end,
-                                         include_start = True,
-                                         include_end = True)
+    series_upsampled = series_upsampled.between_time(sample_start, sample_end, include_start=True,  include_end=True)
     values_not_recorded = series_upsampled[~series_upsampled.index.isin(series_o.index)]
     values_recorded_too_late = series_o[~series_o.index.isin(series_upsampled.index)]
     return values_not_recorded, values_recorded_too_late
+
 
 def number_missing_aquisitions(df_gym, interval='20min', sample_start='07:20', sample_end='23:40'):
     '''
@@ -89,16 +74,12 @@ def number_missing_aquisitions(df_gym, interval='20min', sample_start='07:20', s
     Returns
     -------
     Number of missing elements, an integer
-
     '''
-    values_not_recorded, values_recorded_too_late = get_missing_aquisition_timestamps(df_gym, 
-                                                                                      interval,
-                                                                                      sample_start,
-                                                                                      sample_end)
+    values_not_recorded, values_recorded_too_late = get_missing_aquisition_timestamps(df_gym, interval, sample_start, sample_end)
     print(len(values_not_recorded),' values were not recorded!')
     print(len(values_recorded_too_late),' values were recorded too late!')
     
-    return len(values_not_recorded)+len(values_recorded_too_late)
+    return len(values_not_recorded) + len(values_recorded_too_late)
 
 def add_missing_timestamps(df, interval='20min', sample_start='07:20', sample_end='23:40'):
     '''
@@ -118,23 +99,15 @@ def add_missing_timestamps(df, interval='20min', sample_start='07:20', sample_en
     -------
     df_new : the data frame with the missing timestamps for the gyms added 
     (but all other values being NaN)
-
     '''
-
 
     for gym in df.gym_name.unique():
         #how many values are missing for this gym?
-        df_gym = df[df.gym_name == gym].sort_values(by = 'current_time',
-                                                    ascending=True)
-        values_not_recorded, values_recorded_too_late = get_missing_aquisition_timestamps(df_gym, 
-                                                                                          interval,
-                                                                                          sample_start,
-                                                                                          sample_end)
+        df_gym = df[df.gym_name == gym].sort_values(by='current_time', ascending=True)
+        values_not_recorded, values_recorded_too_late = get_missing_aquisition_timestamps(df_gym, interval, sample_start, sample_end)
         #add the values not recorded
         values_to_append = [{'current_time': time,'gym_name': gym} for time in values_not_recorded.index]
-        df = df.append(values_to_append,
-                       ignore_index=True)
-
+        df = df.append(values_to_append, ignore_index=True)
     return df
 
 def fill_nan_values(df):
@@ -155,15 +128,14 @@ def fill_nan_values(df):
     '''
     #fill the missing values with a nearest interpolation of the same day!
     for gym in df.gym_name.unique():
-        df_gym = df[df.gym_name == gym].sort_values(by = 'current_time',
-                                                    ascending = True)
+        df_gym = df[df.gym_name == gym].sort_values(by='current_time', ascending=True)
         #get the timestamps where values are missing
         timestamps = df_gym.current_time[df_gym.occupancy.isnull()]
         for index, timestamp in timestamps.items():
             #get a series of the day in question
             time_mask = (df_gym.current_time.dt.day == timestamp.day)&\
-                (df_gym.current_time.dt.month == timestamp.month)&\
-                    (df_gym.current_time.dt.year == timestamp.year)
+                        (df_gym.current_time.dt.month == timestamp.month)&\
+                        (df_gym.current_time.dt.year == timestamp.year)
             df_gym_day = df_gym[time_mask]
             #drop the nans of this series
             df_gym_day_noNan = df_gym_day.dropna()
@@ -171,15 +143,10 @@ def fill_nan_values(df):
             #xavlues need to be real valued!
             x_values = (df_gym_day_noNan.current_time-pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
             #only do this if there are enough x_values >5
-            if len(x_values)>5:
+            if len(x_values) > 5:
                 for column in ['occupancy', 'waiting', 'weather_temp']:
                     # this might work with the dev version of scipy:
-                    
-                    func = interp1d(x_values,
-                                    df_gym_day_noNan[column],
-                                    kind = 'linear',
-                                  bounds_error = False, 
-                                  fill_value = 'extrapolate')
+                    func = interp1d(x_values, df_gym_day_noNan[column], kind='linear', bounds_error=False,  fill_value='extrapolate')
                     
                     #fill in the interpolated value!
                     timestamp_unix = (timestamp-pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
@@ -196,21 +163,18 @@ def fill_nan_values(df):
             for column in ['weather_status']:
                 #take as a default the one from before, if it doesn't exist, the one after
                 #add the timestamp to the noNan and sort it
-                df_gym_day_oneNan = df_gym_day_noNan.append(df.loc[index],
-                                                            ignore_index = False)
-                df_gym_day_oneNan = df_gym_day_oneNan.sort_values(by = 'current_time',
-                                                                  ascending = True)
+                df_gym_day_oneNan = df_gym_day_noNan.append(df.loc[index], ignore_index=False)
+                df_gym_day_oneNan = df_gym_day_oneNan.sort_values(by='current_time', ascending=True)
                 #Case 1: it is at the beginning:
                 if df_gym_day_oneNan.iloc[0].isnull().sum() == 1:
-                    shift=-1
+                    shift =- 1
                 else:
-                    shift=+1
+                    shift =+ 1
                 df_gym_day_oneNan_shift = df_gym_day_oneNan.shift(shift)
                 df.loc[index,column] = df_gym_day_oneNan_shift.loc[index, column]
-                
     return df
 
-def remove_excess_values(df, interval = '20min', sample_start = '07:20', sample_end = '23:40'):
+def remove_excess_values(df, interval='20min', sample_start='07:20', sample_end='23:40'):
     '''
     remove rows which where not measured at the set intervals
 
@@ -228,24 +192,19 @@ def remove_excess_values(df, interval = '20min', sample_start = '07:20', sample_
     Returns
     -------
     df : pd dataFrame
-
     '''
     for gym in df.gym_name.unique():
-        df_gym = df[df.gym_name == gym].sort_values(by = 'current_time',
-                                                    ascending = True)
-        values_not_recorded, values_recorded_too_late = get_missing_aquisition_timestamps(df_gym, 
-                                                                                          interval,
-                                                                                          sample_start,
-                                                                                          sample_end)
+        df_gym = df[df.gym_name == gym].sort_values(by='current_time', ascending=True)
+        values_not_recorded, values_recorded_too_late = get_missing_aquisition_timestamps(df_gym, interval, sample_start, sample_end)
         #remove the values_recorded_too_late
         cond1 = (df.current_time.isin(values_recorded_too_late.index))
-        cond2 = (df.gym_name==gym)
+        cond2 = (df.gym_name == gym)
         cond3 = (cond1 & cond2)
         df = df.drop(df[cond3].index)
     return df
 
 
-def correct_bouldering_dataframe(df, interval = '20min', sample_start = '07:20', sample_end = '23:40'):
+def correct_bouldering_dataframe(df, interval='20min', sample_start='07:20', sample_end='23:40'):
     '''
     Recast boulder dataframe into another time-sampling and interpolate the data
 
@@ -263,12 +222,7 @@ def correct_bouldering_dataframe(df, interval = '20min', sample_start = '07:20',
     Returns
     -------
     pandas dataframe
-
     '''
 
-    return remove_excess_values(fill_nan_values(add_missing_timestamps(df)),
-                                interval,
-                                sample_start,
-                                sample_end)
-
+    return remove_excess_values(fill_nan_values(add_missing_timestamps(df)), interval, sample_start, sample_end)
 
