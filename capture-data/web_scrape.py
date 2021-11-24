cc8fcffc135cac3ea21708ee216395d23c23eae1
ane <anebzt@protonmail.com>
2021-11-24 11:44:15 +0100

    get updated times for summer/winter times + round to nearest 20min interval

--- capture-data/web_scrape.py
+++ capture-data/web_scrape.py
@@ -2,12 +2,13 @@ import os
 import re
 import json
 import time
+import pytz
 import boto3
 import pyowm
+import datetime
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
 
 
 def get_occupancy_boulderwelt(url: str) -> tuple():
@@ -58,12 +59,9 @@ def get_weather_info(location: str) -> tuple():
     return temp, status
 
 
-def scrape_websites() -> pd.DataFrame:
+def scrape_websites(current_time: str) -> pd.DataFrame:
 
     webdata = []
-    # Winter time: (datetime.now() + timedelta(hours=1)). Currently +2 because in summer time
-    current_time = (datetime.now() + timedelta(hours=2)).strftime("%Y/%m/%d %H:%M")
-
     for gym_name, gym_data in gyms.items():
         weather_temp, weather_status = get_weather_info(gym_data['location'])
         occupancy, waiting = gym_data['function'](gym_data['url'])
@@ -114,9 +112,41 @@ gyms = {
     }
 }
 
+def get_current_time():
+    # https://stackoverflow.com/a/60169568/4569908
+    dt = datetime.datetime.now()
+    timeZone = pytz.timezone("Europe/Berlin")
+    aware_dt = timeZone.localize(dt)
+    if aware_dt.dst() != datetime.timedelta(0,0):
+        #summer time
+        dt += datetime.timedelta(hours=2)
+    else:
+        #winter time
+        dt += datetime.timedelta(hours=1)
+    dt = dt.strftime("%Y/%m/%d %H:%M")
+
+    #round to the nearest 20min interval
+    minutes = [0, 20, 40]
+    current_min = int(dt.split(':')[1])
+    distances = [abs(current_min - _min) for _min in minutes]
+    closest_min = minutes[distances.index(min(distances))]
+    current_time = dt.replace(':'+str(current_min), ':'+str(closest_min))
+
+    return current_time
+
 
 def lambda_handler(event, context):
-    webdf = scrape_websites()
+
+    current_time = get_current_time()
+    current_time_hh_mm = datetime.datetime.strptime(current_time, '%Y/%m/%d %H:%M').strftime("%H:%M")
+
+    if current_time_hh_mm < '07:00' or current_time_hh_mm > '23:00':
+        print("Scraping outside of opening hours, skipping")
+        return
+    
+    print(f"Current time: {current_time}")
+
+    webdf = scrape_websites(current_time)
 
     # only update if occupancy in gyms is > 0
     if webdf.empty:
@@ -126,12 +156,12 @@ def lambda_handler(event, context):
     # download dataset from S3
     s3 = boto3.client('s3')
     dfpath = f"/tmp/{os.environ['CSVNAME']}"
-
-    # download dataset from S3
     s3.download_file(os.environ['BUCKETNAME'], os.environ['CSVNAME'], dfpath)
 
     # merge boulderdata with tmp file
     webdf.append(pd.read_csv(dfpath)).to_csv(dfpath, index=False)
+
+    # upload dataset to S3
     s3.upload_file(dfpath, os.environ['BUCKETNAME'], os.environ['CSVNAME'])
     print("Scraping done and data updated to S3")
     return
